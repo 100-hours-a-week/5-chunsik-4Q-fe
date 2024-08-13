bd10d89781018c491c3879dd8c615a4377cbb931
moolmin <zzmzzm0065@gmail.com>
2024-08-13 22:17:47 +0900

    Feat: create email login page

--- app/(pages)/login/email/page.tsx
+++ app/(pages)/login/email/page.tsx
@@ -1,64 +1,67 @@
+"use client";
+
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input} from 'antd';
+import type { FormProps } from 'antd';
 import styles from './page.module.css';
+import Link from "next/link";
 
 type FieldType = {
     username?: string;
     password?: string;
-    remember?: string;
+    remember?: boolean; // Make sure remember is a boolean since it relates to the checkbox
 };
 
-// const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
-//     console.log('성공:', values);
-// };
+const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+    console.log('Success:', values);
+};
 
 const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
-    console.log('실패:', errorInfo);
+    console.log('Failed:', errorInfo);
 };
-
-const LoginForm: React.FC = () => (
-    <Form
-        name="login"
-        className={styles.form}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-    >
-        <Form.Item<FieldType>
-            name="username"
-            rules={[{ required: true, message: '사용자 이름을 입력해주세요!' }]}
-        >
-            <Input placeholder="사용자 이름" />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-            name="password"
-            rules={[{ required: true, message: '비밀번호를 입력해주세요!' }]}
-        >
-            <Input.Password placeholder="비밀번호" />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-            name="remember"
-            valuePropName="checked"
-        >
-            <Checkbox>자동 로그인</Checkbox>
-        </Form.Item>
-
-        <Form.Item>
-            <Button type="primary" htmlType="submit">
-                로그인
-            </Button>
-        </Form.Item>
-    </Form>
-);
 
 export default function Page() {
     return (
         <div className={styles.container}>
-            <h1>로그인</h1>
-            <LoginForm />
+            <Form
+                name="login"
+                style={{ maxWidth: 700, width: '80%' }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item<FieldType>
+                    name="username"
+                    rules={[{ required: true, message: '이메일을 입력해주세요.' }]}
+
+                >
+                    <Input  variant="filled" placeholder="이메일" className={styles.inputField} />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                    name="password"
+                    rules={[{ required: true, message: '비밀번호를 입력해주세요.' }]}
+                >
+                    <Input.Password  variant="filled" placeholder="비밀번호" className={styles.inputField} />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                    name="remember"
+                    valuePropName="checked"
+                >
+                    <Checkbox>로그인 상태 유지</Checkbox>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className={styles.submitBtn}>
+                        로그인
+                    </Button>
+                </Form.Item>
+            </Form>
+            <Link href="/signup/email" className={styles.emailLink}>
+                이메일로 회원가입
+            </Link>
         </div>
     );
 }
