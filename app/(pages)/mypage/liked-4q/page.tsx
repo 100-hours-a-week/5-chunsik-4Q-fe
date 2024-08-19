2519f9327e1f3cbe11e734440940fa2941a943f5
moolmin <zzmzzm0065@gmail.com>
2024-08-19 21:26:11 +0900

    feat: add construction indicator in my 4q and liked 4q page

--- app/(pages)/mypage/liked-4q/page.tsx
+++ app/(pages)/mypage/liked-4q/page.tsx
@@ -1,9 +1,16 @@
 import styles from './page.module.css'
+import construction from '../../../../public/images/construction.png'
 
 export default function Page() {
     return (
         <div className={styles.container}>
-            좋아요한 4Q
+            <img src={construction.src} alt="construction"/>
+            <div className={styles.title}>
+               현재 페이지는 <span className={styles.orange}>공사중</span>입니다.
+            </div>
+            <div className={styles.subTitle}>
+                "빠른 시일 내에 오픈하도록 노력하겠습니다."
+            </div>
         </div>
     );
 }
