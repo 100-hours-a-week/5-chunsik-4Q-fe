03f6a6a1064f4ea3eb79b9effc7783e375411a45
moolmin <zzmzzm0065@gmail.com>
2024-08-20 13:27:17 +0900

    style: change login logo image to png from svg

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './page.module.css';
-import logo from '../../../public/images/logo.svg';
+import logo from '../../../public/images/login_logo.png';
 import kakao_icon from '../../../public/images/kakao.svg';
 
 export default function Page() {
