d085fd85af7b88fac3baaed7623c9744b44846dc
moolmin <zzmzzm0065@gmail.com>
2024-08-21 22:53:51 +0900

    feat: apply autocomplete in email input field

--- app/(pages)/signup/email/page.tsx
+++ app/(pages)/signup/email/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Form, Input, Button, message } from "antd";
+import { Form, Input, Button, message, AutoComplete } from "antd";
+import Head from 'next/head';
 
 import styles from "./page.module.css";
 import { requestEmailVerification, verifyEmailCode, requestRegister } from "../../../../service/auth_api";
 import { useRouter } from 'next/navigation';
+
+// const emailDomains = ["gmail.com", "yahoo.com", "outlook.com", "naver.com", "daum.net"];
 
 export default function Signup() {
     const [email, setEmail] = useState("");
@@ -16,15 +19,36 @@ export default function Signup() {
     const [buttonText, setButtonText] = useState("이메일 인증");
     const [valiButtonText, setValiButtonText] = useState("확인");
     const [verificationCode, setVerificationCode] = useState("");
-    const router = useRouter(); 
+    const [options, setOptions] = useState<{ value: string }[]>([]);
+    const router = useRouter();
 
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
+    const handleEmailChange = (value: string) => {
         setEmail(value);
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         setIsEmailValid(emailPattern.test(value));
+
+        if (!value.includes("@")) {
+            setOptions([]);
+        } else {
+            const [localPart, domainPart] = value.split("@");
+            // const filteredDomains = emailDomains
+            //     .filter(domain => domain.startsWith(domainPart))
+            //     .map(domain => ({ value: `${localPart}@${domain}` }));
+            // setOptions(filteredDomains);
+        }
     };
-    
+
+    const handleSearch = (value: string) => {
+        setOptions(() => {
+          if (!value || value.includes('@')) {
+            return [];
+          }
+          return ['gmail.com', 'naver.com', 'daum.net', 'nate.com', 'outlook.com'].map((domain) => ({
+            label: `${value}@${domain}`,
+            value: `${value}@${domain}`,
+          }));
+        });
+      };
 
     const handleEmailVerification = async () => {
         setIsBtnDisable(true);
@@ -65,7 +89,6 @@ export default function Signup() {
             message.error("6자리 인증번호를 입력해주세요.");
         }
     };
-    
 
     useEffect(() => {
         if (isTimerActive && timerCount > 0) {
@@ -122,19 +145,22 @@ export default function Signup() {
                 layout="vertical"
             >
                 <div className={styles.emailFormContainer}>
-                <Form.Item
-                    label="이메일"
-                    name="email"
-                    rules={[{ required: true, message: "이메일을 입력해주세요." }]}
-                    className={styles.emailcontainer}
-                >
-                    <Input
-    placeholder="이메일을 입력해주세요."
-    onChange={handleEmailChange}
-    value={email}
-    className={styles.inputField}
-/>
-                 </Form.Item>
+                    <Form.Item
+                        label="이메일"
+                        name="email"
+                        rules={[{ required: true, message: "이메일을 입력해주세요." }]}
+                        className={styles.emailcontainer}
+                    >
+                        <AutoComplete
+                            options={options}
+                            onChange={handleEmailChange}
+                            onSearch={handleSearch}
+                            placeholder="이메일을 입력해주세요."
+                            value={email}
+                            style={{height: '50px'}}
+                            className={styles.inputField}
+                        />
+                    </Form.Item>
                     <button
                         className={styles.emailBtn}
                         style={{
@@ -147,44 +173,40 @@ export default function Signup() {
                     >
                         {buttonText}
                     </button>
-                    </div>
+                </div>
 
-                    
-                    
-                    <div className={styles.emailFormContainer}>
-                <Form.Item
-                    name="emailVerification"
-                    rules={[{ required: true, message: "이메일 인증 번호를 입력해주세요." }]}
-                    className={styles.emailValiContainer}
-                >
-                    <div className={styles.valiContainer}>
-                        {isTimerActive && (
-                            <span className={styles.timerContainer}>
-                                {formatTime(timerCount)}
-                            </span>
-                        )}
-                        <Input
-                            placeholder="인증번호 6자리를 입력해주세요."
-                            maxLength={6}
-                            value={verificationCode}
-                            onChange={(e) => setVerificationCode(e.target.value)}
-                            className={styles.inputField}
-                        />
-                        
-                       
-                    </div>
-                </Form.Item>
-                 <button
-                            className={styles.emailValiBtn}
-                            style={{
-                                cursor: valiButtonText === "인증 성공" ? "not-allowed" : "pointer"
-                            }}
-                            onClick={handleCodeVerification}
-                            type="button"
-                        >
-                            {valiButtonText}
-                        </button>
-</div>
+                <div className={styles.emailFormContainer}>
+                    <Form.Item
+                        name="emailVerification"
+                        rules={[{ required: true, message: "이메일 인증 번호를 입력해주세요." }]}
+                        className={styles.emailValiContainer}
+                    >
+                        <div className={styles.valiContainer}>
+                            {isTimerActive && (
+                                <span className={styles.timerContainer}>
+                                    {formatTime(timerCount)}
+                                </span>
+                            )}
+                            <Input
+                                placeholder="인증번호 6자리를 입력해주세요."
+                                maxLength={6}
+                                value={verificationCode}
+                                onChange={(e) => setVerificationCode(e.target.value)}
+                                className={styles.inputField}
+                            />
+                        </div>
+                    </Form.Item>
+                    <button
+                        className={styles.emailValiBtn}
+                        style={{
+                            cursor: valiButtonText === "인증 성공" ? "not-allowed" : "pointer"
+                        }}
+                        onClick={handleCodeVerification}
+                        type="button"
+                    >
+                        {valiButtonText}
+                    </button>
+                </div>
                 <Form.Item
                     label="비밀번호"
                     name="password"
