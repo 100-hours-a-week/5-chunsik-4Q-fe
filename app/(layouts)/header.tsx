35b971e10de335e67e09134d7230304c1ef7fbcc
moolmin <zzmzzm0065@gmail.com>
2024-08-27 19:19:14 +0900

    feat: modal for back btn in create page

--- app/(layouts)/header.tsx
+++ app/(layouts)/header.tsx
@@ -9,13 +9,14 @@ import { Slant as Hamburger } from "hamburger-react";
 import { useRouter, usePathname } from "next/navigation";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoLogOutOutline } from "react-icons/io5";
-import { Button, message } from "antd";
+import { Button, message, Modal } from "antd";
 
 export default function Header() {
   const [isOpen, setOpen] = useState<boolean>(false);
   const [isLogo, setLogo] = useState<boolean>(false);
   const [title, setTitle] = useState<string>("홈");
   const [isAuthenticated, setAuthenticated] = useState<boolean>(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const router = useRouter();
   const path = usePathname();
 
@@ -34,12 +35,10 @@ export default function Header() {
   ];
 
   const handleBack = () => {
-    router.back();
-
-    const storageKey = "form_data";
-
-    if (sessionStorage.getItem(storageKey)) {
-      sessionStorage.removeItem(storageKey);
+    if (path === "/4q-create") {
+      setIsModalOpen(true);
+    } else {
+      router.back();
     }
   };
 
@@ -77,6 +76,16 @@ export default function Header() {
     setAuthenticated(!!token);
   };
 
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleConfirmBack = () => {
+    setIsModalOpen(false);
+    sessionStorage.removeItem('form_data');
+    router.back();
+  };
+
   useEffect(() => {
     checkLogo();
     getTitleForPath();
@@ -84,85 +93,104 @@ export default function Header() {
   }, [path]);
 
   return (
-    <div className={styles.container}>
-      {!isLogo ? (
-        <div className={styles.backBtn}>
-          <a onClick={handleBack}>
-            <IoIosArrowBack />
-          </a>
+    <>
+      <div className={styles.container}>
+        {!isLogo ? (
+          <div className={styles.backBtn}>
+            <a onClick={handleBack}>
+              <IoIosArrowBack />
+            </a>
+          </div>
+        ) : (
+          <Link href="/">
+            <img src={logo.src} alt="Logo" className={styles.logo} />
+          </Link>
+        )}
+        <div className={styles.title}>{title}</div>
+        <div className={styles.hamburgerMenu}>
+          <div className={styles.hamburgerBorder}></div>
+          <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
         </div>
-      ) : (
-        <Link href="/">
-          <img src={logo.src} alt="Logo" className={styles.logo} />
-        </Link>
-      )}
-      <div className={styles.title}>{title}</div>
-      <div className={styles.hamburgerMenu}>
-        <div className={styles.hamburgerBorder}></div>
-        <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
-      </div>
-      {isOpen && <div className={styles.dimOverlay} onClick={closeMenu}></div>}
-      <div className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
-        <img src={logo_white.src} alt="Logo" />
-        <ul>
-          <li>
-            <Link href="/" data-replace="홈" onClick={closeMenu}>
-              <span>홈</span>
-            </Link>
-          </li>
-          {!isAuthenticated && (
+        {isOpen && (
+          <div className={styles.dimOverlay} onClick={closeMenu}></div>
+        )}
+        <div className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+          <img src={logo_white.src} alt="Logo" />
+          <ul>
             <li>
-              <Link href="/login" data-replace="로그인" onClick={closeMenu}>
-                <span>로그인</span>
+              <Link href="/" data-replace="홈" onClick={closeMenu}>
+                <span>홈</span>
               </Link>
             </li>
-          )}
-          <li>
-            <Link
-              href="/4q-create"
-              data-replace="4Q 생성하기"
-              onClick={closeMenu}
-            >
-              <span>4Q 생성하기</span>
-            </Link>
-          </li>
-          {/* {isAuthenticated && ( */}
-          <li>
-            <Link href="/mypage" data-replace="마이페이지" onClick={closeMenu}>
-              <span>마이페이지</span>
-            </Link>
-          </li>
-          {/* )} */}
-          <li>
-            <Link
-              href="/feedback"
-              data-replace="피드백주기"
-              onClick={closeMenu}
-            >
-              <span>피드백주기</span>
-            </Link>
-          </li>
-          {/* <li> */}
-          
-          {/* </li> */}
-          {isAuthenticated && (
-            <div className={styles.logoutBtnContainer}>
-            <Link href="/login" onClick={closeMenu}>
-              <Button
-                type="primary"
-                shape="round"
-                icon={<IoLogOutOutline />}
-                // size="large"
-                style={{ backgroundColor: "#D4D4D4", color: "#000" }}
-                onClick={handleLogout}
+            {!isAuthenticated && (
+              <li>
+                <Link href="/login" data-replace="로그인" onClick={closeMenu}>
+                  <span>로그인</span>
+                </Link>
+              </li>
+            )}
+            <li>
+              <Link
+                href="/4q-create"
+                data-replace="4Q 생성하기"
+                onClick={closeMenu}
               >
-                로그아웃
-              </Button>
-            </Link>
-            </div>
-          )}
-        </ul>
+                <span>4Q 생성하기</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/mypage"
+                data-replace="마이페이지"
+                onClick={closeMenu}
+              >
+                <span>마이페이지</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/feedback"
+                data-replace="피드백주기"
+                onClick={closeMenu}
+              >
+                <span>피드백주기</span>
+              </Link>
+            </li>
+            {isAuthenticated && (
+              <div className={styles.logoutBtnContainer}>
+                <Link href="/login" onClick={closeMenu}>
+                  <Button
+                    type="primary"
+                    shape="round"
+                    icon={<IoLogOutOutline />}
+                    style={{ backgroundColor: "#D4D4D4", color: "#000" }}
+                    onClick={handleLogout}
+                  >
+                    로그아웃
+                  </Button>
+                </Link>
+              </div>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
+      <Modal
+        title="뒤로가기"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        centered
+        // width={400}
+        footer={[
+          <Button key="cancel" onClick={handleCancel}>
+            머무르기
+          </Button>,
+          <Button key="back" type="primary" onClick={handleConfirmBack}>
+            그래도 뒤로가기
+          </Button>,
+        ]}
+      >
+        페이지를 이동하면 지금까지 입력했던 정보가 사라집니다.
+      </Modal>
+    </>
   );
 }
