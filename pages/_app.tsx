811af1f3ee1d898a144d79089a6e57ff4bd86d23
moolmin <zzmzzm0065@gmail.com>
2024-08-27 13:52:47 +0900

    feat: add google analytics in root path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,48 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import * as gtag from '../lib/gtag';
-import Script from 'next/script';
+import Head from 'next/head';
+import '../styles/global.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+const App = ({ Component, pageProps }: AppProps) => (
+        <ConfigProvider >
+        <Head>
+    <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
+  </Head>
+        <Component {...pageProps} />
+    </ConfigProvider>
+);
 
-  useEffect(() => {
-    const handleRouteChange = (url: URL) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  return (
-    <>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
-      <Component {...pageProps} />
-    </>
-  );
-};
-
-export default MyApp;
+export default App;
