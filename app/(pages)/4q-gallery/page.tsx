91b556ea5f2bb704d5adb9802f1e5f1d1b55f13b
Sumin Kim <zzmzzm0065@gmail.com>
2024-09-01 20:03:50 +0900

    feat: add validation for password to require special characters

--- app/(pages)/4q-gallery/page.tsx
+++ app/(pages)/4q-gallery/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, Suspense } from "react";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import styles from "./page.module.css";
-import { Select, Input, Modal } from "antd";
+import { Select, Input } from "antd";
 import type { GetProps } from 'antd';
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import "react-horizontal-scrolling-menu/dist/styles.css";
 import { IoIosArrowBack, IoIosArrowForward, IoIosSearch } from "react-icons/io";
-
-import TagSelector from "../4q-create/(modals)/tagSelectModal";
-import tagTranslationMap from '../../../lib/tagTranslationKrEn';
 import Container from "./_components/item-container";
 
 type SearchProps = GetProps<typeof Input.Search>;
@@ -38,12 +35,10 @@ export default function Page() {
   const searchParams = useSearchParams();
   const [selectedCategory, setSelectedCategory] = useState<string>("all");
 
-  // Fetch query parameters from URL
   const categoryParam = searchParams?.get("category") || "all";
   const tagParam = searchParams?.get("tag") || "";
   const sortParam = searchParams?.get("sort") || "latest";
 
-  // createQueryString 함수 정의
   const createQueryString = useCallback(
     (name: string, value?: string) => {
       const params = new URLSearchParams(searchParams ? searchParams.toString() : "");
@@ -51,7 +46,7 @@ export default function Page() {
       if (value) {
         params.set(name, value);
       } else {
-        params.delete(name); // 값이 없으면 해당 파라미터를 삭제
+        params.delete(name);
       }
 
       return params.toString();
@@ -59,19 +54,16 @@ export default function Page() {
     [searchParams]
   );
 
-  // 필터 버튼 클릭
   const handleFilterBtnClick = () => {
     setIsSearchContainerVisible(!isSearchContainerVisible);
   };
 
-  // 카테고리 선택 시 쿼리 파라미터 업데이트
   const handleCategoryClick = (categoryId: string) => {
     setSelectedCategory(categoryId);
     const newQueryString = createQueryString('category', categoryId !== 'all' ? categoryId : undefined);
     router.push(`${pathname}?${newQueryString}`);
   };
 
-  // 태그 검색 시 쿼리 파라미터 업데이트
   const onSearch: SearchProps['onSearch'] = (value) => {
     const newQueryString = createQueryString('tag', value.trim() ? value : undefined);
     router.push(`${pathname}?${newQueryString}`);
@@ -121,8 +113,7 @@ export default function Page() {
           ]}
         />
         <div className={styles.filterBtn} onClick={handleFilterBtnClick}>
-          
-          {tagParam ? <div className={styles.numberSearch}>1</div> : <IoIosSearch className={styles.filterIcon} />} {/* tag가 있을 때만 numberSearch를 표시 */}
+          {tagParam ? <div className={styles.numberSearch}>1</div> : <IoIosSearch className={styles.filterIcon} />}
           <span>검색</span>
         </div>
       </div>
@@ -148,8 +139,9 @@ export default function Page() {
           <Search size="large" placeholder="" allowClear onSearch={onSearch} style={{ width: '100%' }} />
         </div>
       </div>
-      {/* Pass query parameters to Container component */}
-      <Container category={categoryParam} tag={tagParam} sort={sortParam} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Container category={categoryParam} tag={tagParam} sort={sortParam} />
+      </Suspense>
     </div>
   );
 }
