3dc394ce4f73aa66e1e28812f7fc275e7dc7288a
ZZ3n <ndsrhkd@naver.com>
2024-09-12 13:37:47 +0900

    chore: colorful scripts

--- scripts/push-to-ecr.sh
+++ scripts/push-to-ecr.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
 
+# 색상 코드 정의
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # 색상 초기화
+
 ECR_URL=202533511551.dkr.ecr.ap-northeast-2.amazonaws.com
 
 echo "Step 1: Logging into ECR at $ECR_URL"
 aws ecr get-login-password --region ap-northeast-2 --profile default | docker login --username AWS --password-stdin $ECR_URL
 if [ $? -eq 0 ]; then
-    echo "Successfully logged into ECR."
+    echo -e "${GREEN}Successfully logged into ECR.${NC}"
 else
-    echo "Failed to log into ECR." >&2
+    echo -e "${RED}Failed to log into ECR.${NC}" >&2
     exit 1
 fi
 
 echo "Step 2: Building Docker image for platform linux/arm64 with tag chunsik/dev/fe"
 docker build --platform linux/arm64 -t chunsik/dev/fe ..
 if [ $? -eq 0 ]; then
-    echo "Docker image built successfully."
+    echo -e "${GREEN}Docker image built successfully.${NC}"
 else
-    echo "Failed to build Docker image." >&2
+    echo -e "${RED}Failed to build Docker image.${NC}" >&2
     exit 1
 fi
 
 echo "Step 3: Tagging Docker image with ECR URL $ECR_URL"
 docker tag chunsik/dev/fe:latest $ECR_URL/chunsik/dev/fe:latest
 if [ $? -eq 0 ]; then
-    echo "Docker image tagged successfully."
+    echo -e "${GREEN}Docker image tagged successfully.${NC}"
 else
-    echo "Failed to tag Docker image." >&2
+    echo -e "${RED}Failed to tag Docker image.${NC}" >&2
     exit 1
 fi
 
 echo "Step 4: Pushing Docker image to ECR repository $ECR_URL"
 docker push $ECR_URL/chunsik/dev/fe:latest
 if [ $? -eq 0 ]; then
-    echo "Docker image pushed to ECR successfully."
+    echo -e "${GREEN}Docker image pushed to ECR successfully.${NC}"
 else
-    echo "Failed to push Docker image to ECR." >&2
+    echo -e "${RED}Failed to push Docker image to ECR.${NC}" >&2
     exit 1
 fi
 
-echo "All steps completed successfully."
+echo -e "${GREEN}All steps completed successfully.${NC}"
