5324ea721376ed93abb432101cc65f221da5a656
Sumin Kim <zzmzzm0065@gmail.com>
2024-09-12 15:01:50 +0900

    chore: rever to original image tag in gallery page

--- src/app/(pages)/4q-gallery/_components/item-card.tsx
+++ src/app/(pages)/4q-gallery/_components/item-card.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import Image from 'next/image';
+// import Image from 'next/image';
 import styles from "./item-card.module.css";
 import Heart from "@react-sandbox/heart";
 import Detail from "./detail";
@@ -97,21 +97,21 @@ export default function ItemCard({ item }: ItemCardProps) {
           />
         </div>
         <div className={styles.imgContainer}>
-          <Image 
+          {/* <Image 
           width={200}
           height={200}
           src={item.url}
           alt="photo QR"
           placeholder="blur"
-          onClick={showDrawer}/>
+          onClick={showDrawer}/> */}
           
-          {/* <img
+          <img
             width={200}
             height={200}
             src={item.url}
             alt="photo QR"
             onClick={showDrawer}
-          /> */}
+          />
         </div>
       </div>
       <div className={styles.bottomContainer}>
