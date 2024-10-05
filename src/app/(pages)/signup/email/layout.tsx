5c1fe16e70e1e993a6fea68ad9c2ea5878357dae
Sumin Kim <zzmzzm0065@gmail.com>
2024-10-05 12:56:25 +0900

    fix: fix typo in metadata of signup

--- src/app/(pages)/signup/email/layout.tsx
+++ src/app/(pages)/signup/email/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: '4Q | 회원가입',
+  title: '회원가입',
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
