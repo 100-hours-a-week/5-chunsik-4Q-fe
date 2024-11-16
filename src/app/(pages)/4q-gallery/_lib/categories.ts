65ff9bc51047c030dbc1194347f3acb94abda37b
Sumin Kim <zzmzzm0065@gmail.com>
2024-11-16 11:48:51 +0900

    chore: add categories lib

---
+++ src/app/(pages)/4q-gallery/_lib/categories.ts
@@ -0,0 +1,13 @@
+type Category = {
+    id: string;
+    name: string;
+  };
+
+export const categories: Category[] = [
+    { id: "all", name: "전체" },
+    { id: "메뉴판", name: "메뉴판" },
+    { id: "전시회", name: "전시회" },
+    { id: "콘서트", name: "콘서트" },
+    { id: "출입증", name: "출입증" },
+    { id: "청첩장", name: "청첩장" },
+  ];
